38df6b725d6055638d8bbb515d5b8bffdc3e77e3
Rishav Bose <rishavbose365@gmail.com>
2016-09-01 16:43:39 -0400

    Test if C class works

--- C.cpp
+++ C.cpp
@@ -33,13 +33,47 @@ C<T> C<T>::operator* (const C<T> param) {
 
 //Test if the functions work
 int main() {
+	//Test Getters
+	printf("Test Getters on ints:\n");
 
 	C<int> s (3,5);
-	C<int> t (4,2);
 
-	C<int> result = s * t;
-	printf("%i\n", result.getReal());
-	printf("%i\n", result.getComplex());
+	printf("Does %i = %i for the real values: %s.\n",
+		s.getReal(), s.real, s.getReal()
+		== s.real ? "true" : "false");
+
+	printf("Does %i = %i for the imaginary values: %s.\n",
+		s.getComplex(), s.complex, s.getComplex()
+		== s.complex ? "true" : "false");
+
+	//Test using doubles
+	printf("\nTest Getters on doubles:\n");
+
+	C<double> a (3.0,5.9);
+	C<double> b (4.1,2.5);
+
+	printf("Does %f = %f for the real values: %s.\n",
+		a.getReal(), a.real, a.getReal()
+		== a.real ? "true" : "false");
+
+	printf("Does %f = %f for the imaginary values: %s.\n",
+		a.getComplex(), a.complex, a.getComplex()
+		== a.complex ? "true" : "false");
+
+	//Test Addition
+	printf("\nAddition test: \n");
+	C<double> sum = a + b;
+
+	printf("(%f + %f i) + (%f + %f i) = (%f + %f i)\n",
+		a.real, a.complex, b.real, b.complex, sum.real, sum.complex);
+
+	//Test Multiplication
+	printf("\nMultiplication test: \n");
+	C<double> product = a * b;
+
+	printf("(%f + %f i) * (%f + %f i) = (%f + %f i)\n",
+		a.real, a.complex, b.real, b.complex, product.real, product.complex);
+
 
 	return 1;
 }
