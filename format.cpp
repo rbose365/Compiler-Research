d4ae3a6d779bcba6b63570e8c3f36a130f6830e0
Rishav Bose <rishavbose365@gmail.com>
2016-09-24 13:14:25 -0400

    Create a program to convert format of outputs

---
+++ format.cpp
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	char filename[100] = "M2/dim3-deg7.cpp";
+	char newfilename[100] = "new-dim3-deg7.cpp";
+
+	FILE * newFile;
+	newFile = fopen(newfilename, "w");
+
+	ifstream file(filename);
+	char str[255];
+
+	if (!file) {
+		cout << "Can't open file \n";
+		return 0;
+	}
+
+	int i = 1;
+
+	while (file) {
+		string news;
+		file.getline(str, 255);
+		if (file && i == 3) {
+			fprintf(newFile, "%s\n", str);
+		} else if (file && i > 3) {
+	    	string s(str);
+	    	int j = s.find('.');
+	    	if (j != string::npos) {
+	    		news.append(s.substr(0, j));
+	    		news.append("ii*");
+	    		int k = s.find('.', j+1);
+	    		news.append(s.substr(k, s.length() - k - 4));
+	    		news.append(" + ");
+	    		news.append(s.substr(j,k - j - 1));
+	    	} else {
+	    		news.append(s);
+	    	}
+	    	int f = s.find("C ");
+	    	if (f != string::npos) {
+	    		news.replace(f, 1, "C<double>");
+	    	}
+	    	if (s.find("}") != string::npos) {
+	    		fprintf(newFile, "%s\n", news.c_str());
+	    	} else {
+	    		fprintf(newFile, "\t%s\n", news.c_str());
+	    	}
+	    }
+	    i++;
+	}
+
+	file.close();
+
+	return 1;
+}
