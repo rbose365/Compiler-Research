c6498b21e62d1bfd97e35404b037ff5a629314e2
Rishav Bose <rishavbose365@gmail.com>
2016-11-01 16:54:04 -0400

    Solve Compilation issues

--- Dummy_Assembler/Assembler/assembler.cpp
+++ Dummy_Assembler/Assembler/assembler.cpp
@@ -1,42 +1,36 @@
 #include <stdio.h>
+#include <string>
+
 #define c0 3000
-enum {
+#define g1 4000
+#define x1 5000
+
+enum variable_enum {
+	integer = 0,
+	complex = 1,
 	C0 = c0,
 	C1 = c0 + 1,
-	C2 = c0 + 2
-} constant, variable_enum;
-
-#define g1 4000
-enum {
+	C2 = c0 + 2,
 	G1 = g1,
-	G2 = g1 + 1
-} gate, variable_enum;
-
-#define x1 5000
-enum {
+	G2 = g1 + 1,
 	X1 = x1,
 	X2 = x1 + 1
-} input, variable_enum;
+};
 
-enum {
-	integer = 0,
-	complex = 1
-} other_vars, variable_enum;
-
-enum {
+enum operator_type_enum {
 	ADD = 0,
 	SUB = 1,
 	MULT = 2
-} operator_type_enum
+};
 
-enum {
+enum operand_type_enum {
 	input_variable,
 	output_variable,
 	INT,
 	COMPLEX
-} operand_type_enum
+};
 
-Enumerator operand_types {
+enum all_types {
 	ALL_INPUT_VARIABLES = 0,
 	ALL_OUTPUT_VARIABLES = 1,
 	INPUT_IMMEDIATE = 2,
@@ -45,38 +39,34 @@ Enumerator operand_types {
 };
 
 struct statement {
-	variable_enum dst,
-	variable_enum src1_val,
-	operand_type_enum src1_type,
-	string src1_string,
-	variable_enum src2_val,
-	operand_type_enum src2_type,
-	string src2_string,
-	operator_type_enum operator_type
+	variable_enum dst;
+	variable_enum src1_val;
+	operand_type_enum src1_type;
+	std::string src1_string;
+	variable_enum src2_val;
+	operand_type_enum src2_type;
+	std::string src2_string;
+	operator_type_enum operator_type;
+	all_types operand_types;
 };
 
-statement statements[100];
-
-// G1 = C1 * 0.1125; 
-statements[0].dst = G1;
-statements[0].src1_val = C1;
-statements[0].src1_type = input_variable 
-statements[0].src2_string  = “0.1125”;
-statements[0].src2_val = INT; 
-statements[0].src2_type = integer;
-statements[0].operator = MUL_OP;
-statements[0].operand_types = INPUT_IMMEDIATE;
-
-// G2 = G1 *G1 
-statements[1].dst = G2;
-statements[1].src1 = G1;
-statements[1].src1_type = output_variable 
-statements[1].src2 = G1;
-statements[1].src1_type = output_variable;   
-statements[1].operand = MUL_OP; 
-statements[1].operand_types = OUTPUT_VARIABLES; 
-
+struct statement statements[100];
 
 int main() {
-	printf("%i\n", G1);
+	statements[0].dst = G1;
+	statements[0].src1_val = C1;
+	statements[0].src1_type = input_variable;
+	statements[0].src2_string  = "0.1125";
+	statements[0].src2_val = integer; 
+	statements[0].src2_type = INT;
+	statements[0].operator_type = MULT;
+	statements[0].operand_types = INPUT_IMMEDIATE;
+
+	statements[1].dst = G2;
+	statements[1].src1_val = G1;
+	statements[1].src1_type = output_variable;
+	statements[1].src2_val = G1;
+	statements[1].src1_type = output_variable;   
+	statements[1].operator_type = MULT; 
+	statements[1].operand_types = ALL_OUTPUT_VARIABLES; 
 }
