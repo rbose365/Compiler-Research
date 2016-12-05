a70294e1be7047baf49d051ac32a6eb5fcc2bf6b
Rishav Bose <rishavbose365@gmail.com>
2016-12-05 08:40:29 -0500

    Start Addition Implementation

--- Dummy_Assembler/Assembler/assembler.cpp
+++ Dummy_Assembler/Assembler/assembler.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string>
+#include <stdint.h>
 
-#define c0 3000
-#define g1 4000
-#define x1 5000
+#define c0 0x3000
+#define g1 0x4000
+#define x1 0x5000
 
 enum variable_enum {
 	integer = 0,
@@ -16,6 +17,7 @@ enum variable_enum {
 	X1 = x1,
 	X2 = x1 + 1
 };
+int vals[0x5005];
 
 enum operator_type_enum {
 	ADD = 0,
@@ -42,21 +44,64 @@ struct statement {
 	variable_enum dst;
 	variable_enum src1_val;
 	operand_type_enum src1_type;
-	std::string src1_string;
+	int src1_string;
 	variable_enum src2_val;
 	operand_type_enum src2_type;
-	std::string src2_string;
+	int src2_string;
 	operator_type_enum operator_type;
 	all_types operand_types;
 };
 
+
+int emit(statement *curr) {
+
+	int op1;
+	int op2;
+
+	switch(curr->src1_type) {
+		case INT:
+			op1 = curr->src1_string;
+		case input_variable:
+			op1 = vals[curr->src1_val]
+
+	switch(curr->src2_type) {
+		case INT:
+			op2 = curr->src2_string;
+		case input_variable:
+			op2 = vals[curr->src2_val]
+
+	
+
+	}
+
+	// __asm__ ( "movl $10, %eax;"
+ //                "movl $20, %ebx;"
+ //                "addl %ebx, %eax;"
+ //    );
+
+
+	return 0;
+}
+
 struct statement statements[100];
 
 int main() {
+	int NUM_STATEMENTS = 3;
+	int out = 0;
+	int out1 = 0;
+
+	vals[C1] = 1;
+
+    asm( "movl $2, %0;"
+    	 : "=r" ( vals[0] )
+    	);
+    printf("OUT: %i\n", vals[0]);
+
+    //G1 = C1 * .1125
 	statements[0].dst = G1;
 	statements[0].src1_val = C1;
 	statements[0].src1_type = input_variable;
-	statements[0].src2_string  = "0.1125";
+	statements[0].src2_string  = 1;
 	statements[0].src2_val = integer; 
 	statements[0].src2_type = INT;
 	statements[0].operator_type = MULT;
@@ -68,5 +113,15 @@ int main() {
 	statements[1].src2_val = G1;
 	statements[1].src1_type = output_variable;   
 	statements[1].operator_type = MULT; 
-	statements[1].operand_types = ALL_OUTPUT_VARIABLES; 
+	statements[1].operand_types = ALL_OUTPUT_VARIABLES;
+
+	for (int i = 0; i < NUM_STATEMENTS; ++i) {
+		try {
+			emit(&statements[i]);
+		} catch (int err) {
+			printf("An exception occurred at statement %i : %i\n", i, err);
+			return 1;
+		}
+	}
+	return 0;
 }
